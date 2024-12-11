aa72694ed2998404897a62d2ea424cc9508a67ed
Saian Patel <saianp@icloud.com>
2024-12-11 21:38:25 +0000

    Update calculateLapSummaryMetrics.m

--- +PostProcessing/+ProMoD/calculateLapSummaryMetrics.m
+++ +PostProcessing/+ProMoD/calculateLapSummaryMetrics.m
@@ -25,42 +25,35 @@ function lapSummary = calculateLapSummaryMetrics(runStruct)
         avgSteerAngle = mean(lapData.steerAngle);
 
         % Get the maximum steering wheel angles
-        maxSteerAngle_L = max(lapData.steerAngle(lapData.steerAngle < 0));
-        maxSteerAngle_R = max(lapData.steerAngle(lapData.steerAngle > 0));
+        maxSteerAngle = max(lapData.steerAngle);
 
         % Get the minimum steering wheel angle
-        minSteerAngle_L = max(lapData.steerAngle(lapData.steerAngle < 0));
-        minSteerAngle_R = max(lapData.steerAngle(lapData.steerAngle > 0));
+        minSteerAngle = min(lapData.steerAngle);
 
         % Get the average steering wheel angle
         avg_dSteerAngle = mean(lapData.dSteerAngle);
 
         % Get the maximum steering wheel angles
-        max_dSteerAngle_L = max(lapData.dSteerAngle(lapData.steerAngle < 0));
-        max_dSteerAngle_R = max(lapData.dSteerAngle(lapData.steerAngle > 0));
+        max_dSteerAngle = max(lapData.dSteerAngle);
 
         % Get the minimum steering wheel angle
-        min_dSteerAngle_L = min(lapData.dSteerAngle(lapData.steerAngle < 0));
-        min_dSteerAngle_R = min(lapData.dSteerAngle(lapData.steerAngle > 0));
+        min_dSteerAngle = min(lapData.dSteerAngle);
 
         % Populate the array
         lapSummaryArray(i, 1) = lapNumber;
         lapSummaryArray(i, 2) = avgSteerAngle;
-        lapSummaryArray(i, 3) = maxSteerAngle_L;
-        lapSummaryArray(i, 4) = maxSteerAngle_R;
-        lapSummaryArray(i, 5) = minSteerAngle_L;
-        lapSummaryArray(i, 6) = minSteerAngle_R;
-        lapSummaryArray(i, 7) = avg_dSteerAngle;
-        lapSummaryArray(i, 8) = max_dSteerAngle_L;
-        lapSummaryArray(i, 9) = max_dSteerAngle_R;
-        lapSummaryArray(i, 10) = min_dSteerAngle_L;
-        lapSummaryArray(i, 11) = min_dSteerAngle_R;
+        lapSummaryArray(i, 3) = maxSteerAngle;
+        lapSummaryArray(i, 4) = minSteerAngle;
+        lapSummaryArray(i, 5) = avg_dSteerAngle;
+        lapSummaryArray(i, 6) = max_dSteerAngle;
+        lapSummaryArray(i, 7) = min_dSteerAngle;
+
 
 
     end
 
     % Set the column names
-    columnNames = {'lapNumber', 'avgSteerAngle', 'maxSteerAngle_L', 'maxSteerAngle_R', 'minSteerAngle_L', 'minSteerAngle_R', 'avg_dSteerAngle', 'max_dSteerAngle_L', 'max_dSteerAngle_R', 'min_dSteerAngle_L',  'min_dSteerAngle_R'};
+    columnNames = {'lapNumber', 'avgSteerAngle', 'maxSteerAngle', 'minSteerAngle', 'avg_dSteerAngle', 'max_dSteerAngle', 'min_dSteerAngle'};
 
     % Save as a table
     lapSummary = table('Size', [size(lapSummaryArray,1), length(columnNames)], ...
@@ -69,15 +62,11 @@ function lapSummary = calculateLapSummaryMetrics(runStruct)
 
     lapSummary.lapNumber = lapSummaryArray(:,1);
     lapSummary.avgSteerAngle = lapSummaryArray(:,2);
-    lapSummary.maxSteerAngle_L = lapSummaryArray(:,3);
-    lapSummary.maxSteerAngle_R = lapSummaryArray(:,4);
-    lapSummary.minSteerAngle_L = lapSummaryArray(:,5);
-    lapSummary.minSteerAngle_L = lapSummaryArray(:,6);
-    lapSummary.avg_dSteerAngle = lapSummaryArray(:,7);
-    lapSummary.max_dSteerAngle_L = lapSummaryArray(:,8);
-    lapSummary.max_dSteerAngle_R = lapSummaryArray(:,9);
-    lapSummary.min_dSteerAngle_L = lapSummaryArray(:,10);
-    lapSummary.min_dSteerAngle_R = lapSummaryArray(:,11);
+    lapSummary.maxSteerAngle = lapSummaryArray(:,3);
+    lapSummary.minSteerAngle = lapSummaryArray(:,4);
+    lapSummary.avg_dSteerAngle = lapSummaryArray(:,5);
+    lapSummary.max_dSteerAngle = lapSummaryArray(:,6);
+    lapSummary.min_dSteerAngle = lapSummaryArray(:,7);
 
 
 
