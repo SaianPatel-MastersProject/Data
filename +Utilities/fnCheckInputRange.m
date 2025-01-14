dbca01f7ca131401c5496e7ce5e4a3c8f916e3c4
Saian Patel <saianp@icloud.com>
2025-01-14 16:41:54 +0000

    Create fnCheckInputRange.m
    Utility function for checking that data is within bounds

---
+++ +Utilities/fnCheckInputRange.m
@@ -0,0 +1,20 @@
+%% Function to check whether inputs are within training range
+function [bOutOfRange] = fnCheckInputRange(trainingInputs, dataInputs)
+
+    % Create a logical array for checking that the inputs are within the
+    % ranges of the training data
+    bOutOfRange = zeros([size(dataInputs, 1), size(dataInputs, 2)]);
+
+    % Loop through the data by column
+    for i = 1:size(dataInputs, 2)
+
+        % Get the bounds of the training data
+        maxVal = table2array(max(trainingInputs(:,i)));
+        minVal = table2array(min(trainingInputs(:,i)));
+
+        % Check out of bounds data
+        bOutOfRange(:,1) = and(dataInputs(:,i) < minVal , dataInputs(:,i) > maxVal);
+
+    end
+
+end
