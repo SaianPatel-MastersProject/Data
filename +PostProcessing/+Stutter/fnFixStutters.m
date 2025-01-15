49fb2ae8162e402d2930c6dc7c128a3e32a7d26b
Saian Patel <saianp@icloud.com>
2025-01-15 15:47:17 +0000

    Update fnFixStutters.m
    Fix stutters using distance between points and re-interpolating to get a true time-stamp

--- +PostProcessing/+Stutter/fnFixStutters.m
+++ +PostProcessing/+Stutter/fnFixStutters.m
@@ -10,10 +10,30 @@ function fixedData = fnFixStutters(data, stutterIdx, freq)
     % Get the size of the data without stutters
     nEntries = size(fixedData, 1);
 
-    % Create a new timestep
-    t = dt .* (1:nEntries)';
+    % % Create a new timestep
+    % t = dt .* (1:nEntries)';
+    % 
+    % fixedData.time = t;
 
-    fixedData.time = t;
+    t = zeros([nEntries, 1]);
+    for i = 2:nEntries
+
+        u = fixedData.speed(i-1);
+        v = fixedData.speed(i);
+        s = sqrt((fixedData.posX(i) - fixedData.posX(i-1))^2 + (fixedData.posY(i) - fixedData.posY(i-1))^2);
+        t_i = (2 * s) / (u + v);
+        t(i) = t_i;
+
+    end
+
+    % Create a new timestep to interpolate over
+    t = t(2:end);
+    tNew = [0; cumsum(t)];
+    tInterp = tNew + fixedData.time(1);
+    tQuery = (0:dt:tNew(end))' + fixedData.time(1);
+    y = fixedData(:,2:end);
+    y = table2array(y);
+    fixedData = interp1(tInterp, y, tQuery);
 
 
 
