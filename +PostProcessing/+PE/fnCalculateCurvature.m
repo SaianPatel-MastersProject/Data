818163083dd8028e9b9942a3b01350dd1d74e20f
Saian Patel <saianp@icloud.com>
2025-01-26 10:53:38 +0000

    Update fnCalculateCurvature.m
    - Use signed curvature
    - Directly assign kappa
    - Use movmean with 25 points

--- +PostProcessing/+PE/fnCalculateCurvature.m
+++ +PostProcessing/+PE/fnCalculateCurvature.m
@@ -7,23 +7,15 @@ function [kappa, rCurvature] = fnCalculateCurvature(AIW_Data)
     ddx = gradient(dx);
     ddy = gradient(dy);
 
+
     % Compute curvature
-    curvature = abs(dx .* ddy - dy .* ddx) ./ (dx.^2 + dy.^2).^(3/2);
     curvature_signed = (dx .* ddy - dy .* ddx) ./ (dx.^2 + dy.^2).^(3/2);
 
-    % Compute radius of curvature
-    rCurvature = 1 ./ curvature_signed;
-
-    % Re-interpolate rCurvature
-    % rCurvature = interp1((1:100)', rCurvature, (linspace(1,100, 10000))', 'spline');
-
-    % Map straights to inf
-    rCurvature(isinf(rCurvature)) = inf;
-    rCurvature = movmean(rCurvature, 5);
-    % rCurvature(rCurvature > 200) = 200;
-
     % Set kappa
-    kappa = 1 ./ rCurvature;
+    kappa = movmean(curvature_signed, 25);
+
+    % Set rCurvature
+    rCurvature = 1./ kappa;
 
 
 end
