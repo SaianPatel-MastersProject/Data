5c1fffa3844303f6755ce394e780ed4c257f23f8
Saian Patel <saianp@icloud.com>
2025-02-01 16:57:46 +0000

    Create fnPadEndOfLap.m

---
+++ +TrackMaking/fnPadEndOfLap.m
@@ -0,0 +1,30 @@
+%% Function to pad the end of the lap
+function trackPointsPadded = fnPadEndOfLap(trackPoints, threshold)
+
+    % Set x and y
+    x = trackPoints(:,1);
+    y = trackPoints(:,2);
+
+    % Check the distance between the last and first point
+    dEnd = sqrt((x(1) - x(end))^2 + (y(1) - y(end))^2);
+
+    % Only pad if the gap is bigger than 1m
+    if dEnd > threshold
+
+        d = [0; dEnd];
+        dQ = (0 : threshold : dEnd - threshold)';
+        xI = interp1(d, [x(end); x(1)], dQ);
+        yI = interp1(d, [y(end); y(1)], dQ);
+
+        % Pad
+        x = [x; xI];
+        y = [y; yI];
+
+    end
+
+    trackPointsPadded = [x, y];
+
+
+
+
+end
