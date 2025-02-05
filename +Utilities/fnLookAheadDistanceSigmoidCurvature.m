feb82aee18758efec5b39dcbb12870f4f7cf3eca
Saian Patel <saianp@icloud.com>
2025-02-05 15:55:15 +0000

    Create fnLookAheadDistanceSigmoidCurvature.m
    - Function for defining a sigmoid for look ahead distance as a function of curvature.

---
+++ +Utilities/fnLookAheadDistanceSigmoidCurvature.m
@@ -0,0 +1,23 @@
+%% Function to change look-ahead distance as a function of curvature
+function dLook = fnLookAheadDistanceSigmoidCurvature(dMin, dMax, a, kappa0, kappa)
+    
+    % Take the absolute of kappa and get maximum
+    kappa = abs(kappa);
+    kappaMax = max(kappa);
+
+    % Re-do kappa array
+    kappa = linspace(0, kappaMax, 1000);
+
+    % Compute look-ahead distance using the sigmoid function
+    dLook = dMin + (dMax - dMin) ./ (1 + exp(a * (kappa - kappa0)));
+    
+    % Plot results
+    figure;
+    plot(kappa, dLook, 'b', 'LineWidth', 2);
+    xlabel('Curvature \kappa (1/m)');
+    ylabel('Look-Ahead Distance (m)');
+    title('Sigmoid-Based Look-Ahead Distance as a Function of Curvature');
+    grid on;
+
+
+end
