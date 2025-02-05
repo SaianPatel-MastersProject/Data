3cd9b2a669ecedd2cda4a1c7820cef82ecc4cf01
Saian Patel <saianp@icloud.com>
2025-02-05 16:38:59 +0000

    Update fnLookAheadDistanceSigmoidCurvature.m
    - Revert previous change

--- +Utilities/fnLookAheadDistanceSigmoidCurvature.m
+++ +Utilities/fnLookAheadDistanceSigmoidCurvature.m
@@ -1,12 +1,12 @@
 %% Function to change look-ahead distance as a function of curvature
-function dLook = fnLookAheadDistanceSigmoidCurvature(dMin, dMax, a, kappa0, kappa)
+function [dLook, kappa] = fnLookAheadDistanceSigmoidCurvature(dMin, dMax, a, kappa0, kappa)
     
     % Take the absolute of kappa and get maximum
     kappa = abs(kappa);
-    % kappaMax = max(kappa);
-    % 
-    % % Re-do kappa array
-    % kappa = linspace(0, kappaMax, 1000);
+    kappaMax = max(kappa);
+
+    % Re-do kappa array
+    kappa = linspace(0, kappaMax, 1000);
 
     % Compute look-ahead distance using the sigmoid function
     dLook = dMin + (dMax - dMin) ./ (1 + exp(a * (kappa - kappa0)));
