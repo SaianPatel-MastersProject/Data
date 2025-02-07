5f8542110892b632d3998a739ec00ce607796b43
Saian Patel <saianp@icloud.com>
2025-02-07 15:37:46 +0000

    Update fnLookAheadDistanceSigmoidCurvature.m
    - Disable plotting

--- +Utilities/fnLookAheadDistanceSigmoidCurvature.m
+++ +Utilities/fnLookAheadDistanceSigmoidCurvature.m
@@ -12,12 +12,12 @@ function [dLook, kappa] = fnLookAheadDistanceSigmoidCurvature(dMin, dMax, a, kap
     dLook = dMin + (dMax - dMin) ./ (1 + exp(a * (kappa - kappa0)));
     
     % Plot results
-    figure;
-    plot(kappa, dLook, 'b', 'LineWidth', 2);
-    xlabel('Curvature \kappa (1/m)');
-    ylabel('Look-Ahead Distance (m)');
-    title('Sigmoid-Based Look-Ahead Distance as a Function of Curvature');
-    grid on;
+    % figure;
+    % plot(kappa, dLook, 'b', 'LineWidth', 2);
+    % xlabel('Curvature \kappa (1/m)');
+    % ylabel('Look-Ahead Distance (m)');
+    % title('Sigmoid-Based Look-Ahead Distance as a Function of Curvature');
+    % grid on;
 
 
 end
