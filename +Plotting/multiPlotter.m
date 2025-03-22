61d157d59b222fc460e695236b23934311a0ecfb
Saian Patel <saianp@icloud.com>
2025-03-22 11:53:30 +0000

    Update multiPlotter.m
    Change Envelopes colours

--- +Plotting/multiPlotter.m
+++ +Plotting/multiPlotter.m
@@ -1100,10 +1100,10 @@ classdef multiPlotter
             hold on;
             
             % Plot min-max envelope
-            patch([sLap; flip(sLap)], [meanValues + stdValues; flip(meanValues - stdValues)], 'b', 'FaceAlpha', 0.4)
+            patch([sLap; flip(sLap)], [meanValues + stdValues; flip(meanValues - stdValues)], 'k', 'FaceAlpha', 0.4)
 
             % Plot mean ± standard deviation envelope
-            patch([sLap; flip(sLap)], [minValues; flip(maxValues)], 'b', 'FaceAlpha', 0.2)
+            patch([sLap; flip(sLap)], [minValues; flip(maxValues)], 'k', 'FaceAlpha', 0.2)
 
             % Plot mean values
             plot(sLap, meanValues, 'k--', 'LineWidth', 2, 'DisplayName', 'Average');
