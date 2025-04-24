af9ebce3f5a583cdca7a1e067a6f9b52859e955f
Saian Patel <saianp@icloud.com>
2025-04-24 15:55:14 +0100

    Update sLapWindLateralVelocity.m
    Include induced yaw

--- +ExternalDisturbance/sLapWindLateralVelocity.m
+++ +ExternalDisturbance/sLapWindLateralVelocity.m
@@ -14,33 +14,34 @@ dY = [0; diff(yRef)];
 d = sqrt( dX.^2 + dY.^2);
 sLap = cumsum(d);
 
-%% Specify lateral velocity change at given sLap [sLap, lateralVelocity]
+%% Specify lateral velocity change at given sLap [sLap, lateralVelocity, addedYaw] (right is -ve, left is +ve)
 windProfile = [
-    0, 10; % SF
-    700, 10;
-    800, 0;
-    900, 0;
-    1000, 0;
-    1600, 0;
-    1800, 0;
-    2000, 0;
-    2200, 0;
-    2400, 0;
-    2600, 0;
-    2800, 0;
-    3000, 0;
-    3400, 0;
-    3600, 0;
-    3700, 0;
-    3900, 0;
-    4000, 0;
-    4200, 0;
-    4400, 10;
-    sLap(end), 10;
+    0, 1, 0.015; % SF
+    700, 1, 0.015;
+    800, 0, 0;
+    900, 0, 0;
+    1000, 0, 0;
+    1600, 0, 0;
+    1800, 0, 0;
+    2000, 0, 0;
+    2200, 0, 0;
+    2400, 0, 0;
+    2600, 0, 0;
+    2800, 0, 0;
+    3000, 0, 0;
+    3400, 0, 0;
+    3600, 0, 0;
+    3700, 0, 0;
+    3900, 0, 0;
+    4000, 0, 0;
+    4200, 0, 0;
+    4400, 1, 0.015;
+    sLap(end), 1, 0.015;
 ];
 
 %% Interpolate
 windProfileInterp = interp1(windProfile(:,1), windProfile(:,2), sLap, 'linear');
+yawProfileInterp = interp1(windProfile(:,1), windProfile(:,3), sLap, 'linear');
 
 %% Tie the crosswind to the AIW_Data
 dXNorm = dX ./ d;
@@ -57,7 +58,7 @@ nX_V = nX .* windProfileInterp;
 nY_V = nY .* windProfileInterp;
 
 %% Format the lateralVelocity Profile
-lateralVelocityProfile = [nX_V, nY_V, zeros([numel(nX_V), 1])];
+lateralVelocityProfile = [nX_V, nY_V, zeros([numel(nX_V), 1]), yawProfileInterp];
 %% Plot the wind profile
 figure;
 grid on;
