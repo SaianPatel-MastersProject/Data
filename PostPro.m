a278d4c5795c98ef41907fdb1e240bce63c17dc7
Saian Patel <saianp@icloud.com>
2025-05-05 11:59:12 +0100

    Update PostPro.m

--- PostPro.m
+++ PostPro.m
@@ -1,5 +1,5 @@
 %% PostPro
-matFilePath = 'D:\Users\Saian\Workspace\Data\+ProcessedData\2025\FYP03_24\2025_FYP03_24_D2_R15.mat';
+matFilePath = 'D:\Users\Saian\Workspace\Data\+ProcessedData\2025\FYP04_28\2025_FYP04_28_D7_R09.mat';
 
 interpType = 'Distance';
 interpParam = 0.1;
